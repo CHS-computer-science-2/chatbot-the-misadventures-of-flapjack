0f5fcba5cd73e6c7b9b31e7be4ce014e65d8f9c3
ewodrich0277 <47000828+ewodrich0277@users.noreply.github.com>
2019-04-03 09:05:20 -0500

    Add files via upload

--- Fred.py
+++ Fred.py
@@ -1,19 +1,22 @@
 from textblob import TextBlob
+import random
+baby = open("script.txt","r")
+greetings = ["hi","hello","hey","how are you","whats up","its good to see you"]
+aQue = ["how are you doing","how are you","are you doing well","whats been good"]
+aAdj = ["good","bad","well","terrible","great","horrible","excellent","fine","swell","ok"]
+
 
 def response(userinput):
+    if userinput in greetings:
+        return TextBlob(closest(greetings))
     
-    greetings = ["hi","hello","hey","how are you","whats up","its good to see you"]
-    aQue = ["how are you doing","how are you","are you doing well","whats been good"]
-    aAdj = ["good","bad","well","terrible","great","horrible","excellent","fine","swell","ok"]
-
+def closest(userinput):
+    for 
     
-    
-    return TextBlob(userinput)
-
 def main():
-
      while True:
         userInput = input(" ")
         input(response(userInput))
+        
         
 main()
