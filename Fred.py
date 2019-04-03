6cb888c15a5a8e600ed301b20678ef98775f9b4c
sfabbri <47635154+shanefabbri@users.noreply.github.com>
2019-04-03 07:59:19 -0500

    Update Fred.py

--- Fred.py
+++ Fred.py
@@ -1,13 +1,19 @@
 from textblob import TextBlob
 
 def response(userinput):
+    
     greetings = ["hi","hello","hey","how are you","whats up","its good to see you"]
     aQue = ["how are you doing","how are you","are you doing well","whats been good"]
-    aAdj = ["good","bad","well","terrible","horrible","great","fine","ok","excellent","swell"]
+    aAdj = ["good","bad","well","terrible","great","horrible","excellent","fine","swell","ok"]
+
+    
+    
     return TextBlob(userinput)
 
 def main():
-    while True:
+
+     while True:
         userInput = input(" ")
-        print(response(userInput))
+        input(response(userInput))
+        
 main()
