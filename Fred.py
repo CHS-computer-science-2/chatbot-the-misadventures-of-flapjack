05967a3472bd7f9b5b9e8a08a075a8c0e82d9c60
sfabbri <47635154+shanefabbri@users.noreply.github.com>
2019-04-03 07:48:42 -0500

    Update Fred.py

--- Fred.py
+++ Fred.py
@@ -1,4 +1,22 @@
-if False:
-    print("The hills have eyes")
-else:
-    print("The hills are blind")
+from textblob import TextBlob
+
+def response(userinput):
+    
+    greetings = ["hi","hello","hey","how are you","whats up","its good to see you"]
+    aQue = ["how are you doing","how are you","are you doing well","whats been good"]
+    aAdj = ["good","bad","well","terrible","horrible","great","fine","ok","excelent","swell"]
+
+
+    
+    return TextBlob(userinput)
+
+def main():
+
+    greetings = ["hi","hello","hey","how are you","whats up","its good to see you"]
+    aQue = ["how are you doing","how are you","are you doing well","whats been good"]
+    aAdj = ["good","bad","well","terrible","horrible","great","fine","ok","excelent","swell"]
+    
+    while True:
+        userInput = input(" ")
+        print(response(userInput))
+main()
