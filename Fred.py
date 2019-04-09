76f6bfab26f1eda0a71d6fd8923431e4d8006b11
ewodrich0277 <47000828+ewodrich0277@users.noreply.github.com>
2019-04-09 08:58:59 -0500

    Add files via upload

--- Fred.py
+++ Fred.py
@@ -1,19 +1,19 @@
 from textblob import TextBlob
+greetings = ["hi","hello","hey","sup"]
+aQue = ["how are you doing","how are you","are you doing well","whats been good"]	
+aAdj = ["good","bad","well","terrible","great","horrible","excellent","fine","swell","ok"]
 
-def response(userinput):
-    
-    greetings = ["hi","hello","hey","how are you","whats up","its good to see you"]
-    aQue = ["how are you doing","how are you","are you doing well","whats been good"]
-    aAdj = ["good","bad","well","terrible","great","horrible","excellent","fine","swell","ok"]
-
-    
-    
-    return TextBlob(userinput)
+def response(userInput):
+     UIT = TextBlob(userInput)
+     return 
 
 def main():
-
-     while True:
-        userInput = input(" ")
+    while True:
+        userInput = input("\n")
         input(response(userInput))
-        
+
+
+
+
+
 main()
